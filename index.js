7c8675d756fdc3b5045e0c6c06eebdd5f2bf8fb3
levideutsch <levideutsch@live.com>
2022-10-02 15:18:24 -0400

    commit tree d6894b64f55c11cf59ef71ca0bbfbe07de619460

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
+const input = document.getElementById('button')
+
 function addingEventListener() {
+//   alert("i was clicked")
+  input.addEventListener("click", function(){
+    alert("i was clicked")
+})
 }
+
